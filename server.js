5dd3591412de1b977fa8d37b2699dc1ea4af1889
bherth <bherthchavez@gmail.com>
2022-10-02 12:47:45 +0800

    removed .env file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(sesion({
-    secret: process.env.HIMA,
+    secret: "himawariuzumaki",
     saveUninitialized: true,
     resave: false
 }));  
